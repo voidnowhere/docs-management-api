30871d71c11d90dbc1adcc0d06a62aee0b12bf85
voidnowhere <alivnw@outlook.com>
2024-04-15 09:53:10 +0100

    init

---
+++ src/main/java/fr/norsys/docmanagement/DocManagementApplication.java
@@ -0,0 +1,13 @@
+package fr.norsys.docmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/norsys/docmanagement/DocManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.norsys.docmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
