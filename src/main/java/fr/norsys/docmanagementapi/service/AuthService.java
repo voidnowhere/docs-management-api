426e00527f1845103c76feb83d40e51eb42d6328
HansLanda14ib <bibzazne@gmail.com>
2024-04-19 12:40:26 +0100

    feat : get users for specific realm

--- src/main/java/fr/norsys/docmanagementapi/service/AuthService.java
+++ src/main/java/fr/norsys/docmanagementapi/service/AuthService.java
@@ -1,16 +1,9 @@
 package fr.norsys.docmanagementapi.service;
 
-
-import fr.norsys.docmanagementapi.dto.UserResponse;
-import lombok.AllArgsConstructor;
-import org.keycloak.admin.client.Keycloak;
-import org.keycloak.representations.idm.UserRepresentation;
-import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Component;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -23,15 +16,5 @@ public class AuthService implements IAuthenticationFacade {
 
     public UUID getCurrentUserId() {
         return UUID.fromString(getAuthentication().getName());
-    }
-
-    public List<UserResponse> getAllUsers() {
-        List<UserRepresentation> users = keycloak.realm(REALM_NAME).users().list();
-        return users.stream().map(userRepresentation -> UserResponse.builder()
-                        .id(UUID.fromString(userRepresentation.getId()))
-                        .email(userRepresentation.getEmail())
-                        .build())
-                .toList();
-
     }
 }
